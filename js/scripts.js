c62a811882f6c2c3d5ad0467e3ca044e15553719
Bryce Benson <bryce.e.benson@gmail.com>
2015-11-16 13:53:32 -0800

    updated content and file cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,17 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function(event) {
 
     'use strict';
 
     // Trigger nav
-    var trigger = document.querySelector('.trigger');
-    nav = document.querySelector('.nav');
+    var trigger = document.querySelector('.trigger'),
+        nav = document.querySelector('.nav');
 
     trigger.addEventListener('click', navToggle, false);
 
-    function navToggle() {
-        alert('trigger');
-        if (nav.style.display = 'none') {
-            nav.style.display = 'block';
-        } else {
-            nav.style.display = 'none';
-        }
-        if (this)
+    function navToggle(event) {
+        event.preventDefault();
+        nav.classList.toggle('show');
+        this.classList.toggle('open');
     }
 
-
-
-    // $(".trigger").on('click', function(e) {
-    //     $('.nav').slideToggle();
-    //     e.preventDefault();
-    // })
-
-})
-
+});
